828f31c477e7dd11fa0a6412ffd2996a81af4dba
Josep <josepcortes6@gmail.com>
2022-09-10 13:11:56 +0200

    WIP

--- src/views/Create-Event.jsx
+++ src/views/Create-Event.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import toast from 'react-hot-toast';
 import axios from 'axios';
 
-export default function CreateTraining(){
+export default function CreateEvent(){
     const[event, setEvent] = useState({
         name:'',
         image:'',
