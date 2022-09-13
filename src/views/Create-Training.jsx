9de1f2b45e59f14f559169340923eefd66ea2da8
Josep <josepcortes6@gmail.com>
2022-09-13 20:15:56 +0200

    WIP

--- src/views/Create-Training.jsx
+++ src/views/Create-Training.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 export default function CreateTraining(){
     const[training, setTraining] = useState({
         name:'',
-        image:'',
+        imageUrl:'',
         date:'',
         category:'',  
     })
@@ -27,9 +27,27 @@ export default function CreateTraining(){
         try {
           const newTraining = await axios.post('http://localhost:8000/api/v1/training', training)
           toast.success('Project created successfully')
-          navigate(`/training/${newTraining.data.data._id}`)
+          navigate(`/training`)
         } catch (error) {
           console.error(error);
+        }
+      }
+      const handleFileUpload = async(e) => {
+        const uploadData = new FormData();
+        uploadData.append("imageUrl", e.target.files[0]);
+        try {
+          const response = await axios.post('http://localhost:8000/api/v1/training/upload', uploadData);
+          console.log(response.data.fileUrl);
+    
+          setTraining(prev => {
+            return {
+              ...prev,
+              imageUrl: response.data.fileUrl
+            }
+          })
+        }
+        catch(error){
+          console.error(error)
         }
       }
 
@@ -42,7 +60,8 @@ export default function CreateTraining(){
 
         <label>Nombre</label>
         <input type="text" name="name" placeholder="Nombre" value={training.name} onChange={handleChange}/>
-        <input type="text" name="image" placeholder="URL de la imagen " value={training.image} onChange={handleChange} />
+        <label>Imagen</label>
+        <input type="file" name="image"  value={training.image} onChange={handleFileUpload} />
         <label>Hora de entreno</label>
         <input type="text" name="date" placeholder="Hora " value={training.date} onChange={handleChange} />
         <label>Categoria</label>
