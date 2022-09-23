94adec129921868ace7fe0b74bb40a5a9a27ad25
Josep <josepcortes6@gmail.com>
2022-09-23 19:40:18 +0200

    pending views

--- src/views/TrainingDetail.jsx
+++ src/views/TrainingDetail.jsx
@@ -1,49 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import {useParams } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { AuthContext } from "../context/AuthContext";
-
 export default function TrainingDetail(){
     const[training, setTraining] = useState()
     const{id} = useParams()
     const storedToken = localStorage.getItem('authToken')
-    //const navigate = useNavigate();
     const [isAttending, setAttending] = useState(false);
     const { user } = useContext(AuthContext)
-
-
   useEffect(() => {
         const data = async () =>{
             try {
                 const response =  await axios.get(`http://localhost:8000/api/v1/training/${id}`)
               setTraining(response.data.data);
               const filtered = response.data.data.usersAttending.filter(elem => elem._id === user._id);
-              console.log(filtered);
-              console.log(response.data.data)
               if (filtered.length > 0) {
-                console.log('User is in array')
                 setAttending(true)
               }
-              console.log(isAttending)
             } catch (error) {
                 console.error(error)
-                 
             }
         }
         data()
     },[id, isAttending])
-
-    const handleChange = (e) => {
-        setTraining(prev => {
-          return {
-            ...prev,
-            [e.target.name]: e.target.value
-          }
-        })
-        console.log(training)
-      }
-    
     const handleUser = async(e) => {
        e.preventDefault();
         try {
@@ -54,7 +34,6 @@ export default function TrainingDetail(){
           console.error(error)
        }
     }
-
    const handleUserDeleted = async(e) => {
        e.preventDefault();
        try {
@@ -65,29 +44,30 @@ export default function TrainingDetail(){
            console.error(error)
        }
    }
- 
-  
-       
-
     return (
-        <div>
-          {training && (
-            <div>
-                <h1>{training.name}</h1>
-                <img src={training.imageUrl} alt="" />
-            <h2>{training.date}</h2>
-            <ul>
-              {training && training.usersAttending.map(user => {
-                return <li key={user._id}><img src={user.imageUrl} alt={user.username}/></li>
-                  })}
-            </ul>
-            {!isAttending && <button onClick={handleUser}> ENTRENAR</button>}
-            {isAttending && <button onClick={handleUserDeleted}> NO ASISTIR</button>}
-               
-            </div>   
-          )}
-        </div>
+        <>
+        {training && 
+        <>
+        <h1>{training.name}</h1>
+        <img src={training.imageUrl} alt="" />
+    <h2>{training.date}</h2></>}
+        <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {training && training.usersAttending.map((person) => (
+        <li
+          key={person._id}
+          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+        >
+          <div className="flex flex-1 flex-col p-8">
+            <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src={person.imageUrl} alt="" />
+            <h3 className="mt-6 text-sm font-medium text-gray-900">{person.username}</h3>
+          </div>
+          <div>
+          </div>
+        </li>
+      ))}
+    </ul>
+    {!isAttending && <button onClick={handleUser}> ENTRENAR</button>}
+    {isAttending && <button onClick={handleUserDeleted}> NO ASISTIR</button>}
+    </>
       )
-
-
 }
